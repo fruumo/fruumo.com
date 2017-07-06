60ae9540f4480ae902c2b9fb87a1032f256d84f8
Manak <manak.kapoor@holmusk.com>
2017-07-06 11:08:22 -0400

    New website initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+	module: {
+		rules: [
+		{
+	        test: /\.scss$/,
+			use: ExtractTextPlugin.extract({
+				fallback: "style-loader",
+				use: ['css-loader', 'sass-loader']
+			})
+		}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin("./[name].bundle.css"),
+	],
+	context: path.resolve(__dirname, './precompile/'),
+	entry: {
+		index: './js/index.js'
+	},
+	output: {
+		path: path.resolve(__dirname, './dist/'),
+		filename: '[name].bundle.js',
+	},
+};
+
